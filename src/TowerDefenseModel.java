1b90ed169df78ac3002b7b5e806846a451afc770
Luke <lacernetic@email.arizona.edu>
2019-11-23 18:52:03 -0700

    Working on model

--- src/TowerDefenseModel.java
+++ src/TowerDefenseModel.java
@@ -2,5 +2,20 @@ import java.util.ArrayList;
 
 public class TowerDefenseModel {
 
-	private ArrayList<ArrayList<Tower>> map;
+	private ArrayList<ArrayList<Tower>> towerMap;
+	private ArrayList<ArrayList<Integer>> road;
+	
+	public TowerDefenseModel() {
+		for (int i = 0; i < 13; i++) {
+			ArrayList<Tower> temp = new ArrayList<Tower>();
+			towerMap.add(temp);
+		}
+		
+		for (int i = 0; i < 13; i++) {
+			ArrayList<Integer> temp = new ArrayList<Integer>();
+			for (int j = 0; j < 15; j++) {
+				if (j ==)
+			}
+		}
+	}
 }
