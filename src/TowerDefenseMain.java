e365fc65e46392a5edc74f619964fd6ce9a90829
Luke <lacernetic@email.arizona.edu>
2019-11-23 18:35:52 -0700

    Added class skeleton

--- src/TowerDefenseMain.java
+++ src/TowerDefenseMain.java
@@ -1,5 +1,5 @@
 
-public class towerDefenseMain {
+public class TowerDefenseMain {
 
 	public static void main(String[] args) {
 		
---
+++ src/TowerDefenseModel.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class TowerDefenseModel {
+
+	private ArrayList<ArrayList<Tower>> map;
+}
